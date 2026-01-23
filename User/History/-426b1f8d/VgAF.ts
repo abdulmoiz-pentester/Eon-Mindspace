36b4344dec4ec43fab0bf79281fc63be0b96c78b
abdulmoiz-pentester <abdul.moiz@eonhealth.com>
2026-01-23 16:00:36 +0500

    unnecessary files deleted

--- User/History/-426b1f8d/VgAF.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getBedrockAgentResponse } from "../controllers/bedrockController";
-
-const router = Router();
-
-router.post("/bedrock-agent", getBedrockAgentResponse);
-
-export default router;
